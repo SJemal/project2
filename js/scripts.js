17f3d2cad6a71cd43da195b8bc2fe632a67e3782
Sjemal <jemal0410@gmail.com>
2021-06-04 13:35:10 -0700

    1st line debugged

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  const q1=$(question1).val();
+  const q2=$(question2).val();
+  const q3=$(question3).val();
+  const q4=$(question4).val();
+  const q5=$(question5).val();
+
+if(q5=="4"){
+  $("result").show();}
+else{
+  $("result1").show();
+}
+  });
