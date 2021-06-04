8c121a7cc57a40933503011d3ae5186a0f592610
Sjemal <jemal0410@gmail.com>
2021-06-04 15:52:54 -0700

    there is a code error

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,20 @@
-$(document).ready(function(){
-  var c=0;
-  const q1=$("question1").val();
-  const q2=$("question2").val();
-  const q3=$("question3").val();
-  const q4=$("question4").val();
-  const q5=$(question5).val();
-  if(q1=="Python"){c++}
-  if(q2=="5"){c++}
-  if(q3=="Bootsrap"){c++}
-  if(q4=="Windows"){c++}
-  if(q5=="6 month"){c++}
+$(dokument).ready(function(){
+var c=0;
+const q1=$("question1").val();
+const q2=$("question2").val();
+const q3=$("question3").val();
+const q4=$("question4").val();
+const q5=$(question5).val();
+if(q1=="Python"){c++}
+if(q2=="5"){c++}
+if(q3=="Bootsrap"){c++}
+if(q4=="Windows"){c++}
+if(q5=="6 month"){c++}
 
-  if(c<=3){
-  $("result").show();}
-  else{
-  $("result1").show();
-  }
-  });
+if(c<=3){
+$("result").show();}
+else{
+$("result1").show();
+}
+});
+
